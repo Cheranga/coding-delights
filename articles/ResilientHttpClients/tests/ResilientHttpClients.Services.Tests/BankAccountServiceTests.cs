6bb75b257f5cadfe555a3ab359810d0507eb5836
Cheranga Hatangala <cheranga@gmail.com>
2025-03-10 22:24:44 +1100

    refactor: refactoring BankAccountServiceTests

--- articles/ResilientHttpClients/tests/ResilientHttpClients.Services.Tests/BankAccountServiceTests.cs
+++ articles/ResilientHttpClients/tests/ResilientHttpClients.Services.Tests/BankAccountServiceTests.cs
@@ -1,150 +1,43 @@
-﻿using System.Net;
-using System.Net.Http.Json;
-using System.Text;
+﻿using System.Text;
 using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Http.Resilience;
 using Moq;
-using Polly;
-using ResilientHttpClients.Services.Models;
-using RichardSzalay.MockHttp;
-using WireMock;
-using WireMock.RequestBuilders;
-using WireMock.ResponseBuilders;
-using WireMock.ResponseProviders;
-using WireMock.Server;
-using WireMock.Types;
-using WireMock.Util;
 
 namespace ResilientHttpClients.Services.Tests;
 
-public class BankAccountServiceTests
+public partial class BankAccountServiceTests
 {
     [Fact]
-    public async Task Test1()
-    {
-        var capturedRequests = new List<IRequestMessage>();
-        var wireMockServer = WireMockServer.Start();
-
-        wireMockServer
-            .Given(Request.Create().WithPath("/api/token"))
-            .RespondWith(Response.Create().WithStatusCode(HttpStatusCode.OK).WithBody("new-token"));
-
-        var calledTokens = new List<string>();
-        var mockedCache = new Mock<IDistributedCache>();
-        mockedCache
-            .Setup(x => x.GetAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync("old-token"u8.ToArray());
-        mockedCache
-            .Setup(x =>
-                x.SetAsync(
-                    It.IsAny<string>(),
-                    It.IsAny<byte[]>(),
-                    It.IsAny<DistributedCacheEntryOptions>(),
-                    It.IsAny<CancellationToken>()
-                )
-            )
-            .Callback<string, byte[], DistributedCacheEntryOptions, CancellationToken>(
-                (_, value, _, _) =>
-                {
-                    calledTokens.Add(Encoding.UTF8.GetString(value));
-                }
-            );
-
-        var services = new ServiceCollection();
-        services.AddSingleton(mockedCache.Object);
-
-        services.AddSingleton<ITokenService, TokenService>();
-        services.AddSingleton<TokenHeaderMiddleware>();
-
-        services.AddHttpClient(
-            "tokenservice",
-            client =>
+    public async Task Test1() =>
+        await Arrange(() =>
             {
-                client.BaseAddress = new Uri(wireMockServer.Urls[0]);
-            }
-        );
+                SetupTokenRequestResponses(_wireMockServer);
+                SetupOrdersRequestResponses(_wireMockServer);
+                SetupMockedCache(_mockedCache);
 
-        wireMockServer
-            .Given(Request.Create().WithPath("/api/accounts"))
-            .InScenario(1)
-            .WillSetStateTo(1)
-            .RespondWith(Response.Create().WithStatusCode(HttpStatusCode.Unauthorized));
-
-        wireMockServer
-            .Given(Request.Create().WithPath("/api/accounts"))
-            .InScenario(1)
-            .WhenStateIs(1)
-            .RespondWith(
-                Response
-                    .Create()
-                    .WithStatusCode(HttpStatusCode.OK)
-                    .WithBodyAsJson(
-                        new ListBankAccountsResponse
-                        {
-                            BankAccounts = new List<BankAccountResponse>
-                            {
-                                new()
-                                {
-                                    AccountName = "Bruce Wayne",
-                                    AccountNumber = "1234567890",
-                                    Balance = 5000000,
-                                },
-                            },
-                        }
+                return GetBankAccountService();
+            })
+            .Act(async data => await data.ListBankAccountsAsync(CancellationToken.None))
+            .Assert(result => result.BankAccounts.Count == 1)
+            .And(
+                (_, _) =>
+                    _mockedCache.Verify(
+                        x => x.GetAsync("ApiToken", It.IsAny<CancellationToken>()),
+                        Times.Once
                     )
-            );
-
-        services
-            .AddHttpClient<IBankAccountService, BankAccountService>(client =>
-                client.BaseAddress = new Uri(wireMockServer.Urls[0])
             )
-            .AddHttpMessageHandler<TokenHeaderMiddleware>()
-            .AddResilienceHandler(
-                "retryPipeline",
-                (builder, context) =>
+            .And(
+                (_, _) =>
                 {
-                    var tokenService = context.ServiceProvider.GetRequiredService<ITokenService>();
-
-                    builder.AddRetry(
-                        new HttpRetryStrategyOptions
-                        {
-                            MaxRetryAttempts = 1,
-                            BackoffType = DelayBackoffType.Linear,
-                            Delay = TimeSpan.FromSeconds(1),
-                            ShouldHandle = args =>
-                                args.Outcome.Result is { StatusCode: HttpStatusCode.Unauthorized }
-                                    ? PredicateResult.True()
-                                    : PredicateResult.False(),
-                            OnRetry = async _ =>
-                            {
-                                await tokenService.GetTokenAsync(CancellationToken.None, true);
-                            },
-                        }
+                    _mockedCache.Verify(
+                        x =>
+                            x.SetAsync(
+                                "ApiToken",
+                                Encoding.UTF8.GetBytes("new-token"),
+                                It.IsAny<DistributedCacheEntryOptions>(),
+                                It.IsAny<CancellationToken>()
+                            ),
+                        Times.Once
                     );
                 }
             );
-
-        var response = await services
-            .BuildServiceProvider()
-            .GetRequiredService<IBankAccountService>()
-            .ListBankAccountsAsync(CancellationToken.None);
-
-        // Assert that "calledTokens" contains "old-token" and "new-token" in order
-        mockedCache.Verify(
-            x => x.GetAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()),
-            Times.Once
-        );
-        mockedCache.Verify(
-            x =>
-                x.SetAsync(
-                    It.IsAny<string>(),
-                    It.IsAny<byte[]>(),
-                    It.IsAny<DistributedCacheEntryOptions>(),
-                    It.IsAny<CancellationToken>()
-                ),
-            Times.Once
-        );
-        Assert.Equal(new[] { "new-token" }, calledTokens);
-    }
 }
