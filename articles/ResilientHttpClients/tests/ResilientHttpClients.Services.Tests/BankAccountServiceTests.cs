0b5bd72ca46d3c1dd4511a9d7930446754e6691c
Cheranga Hatangala <cheranga@gmail.com>
2025-03-10 14:00:56 +1100

    chore: tokenservice tests

---
+++ articles/ResilientHttpClients/tests/ResilientHttpClients.Services.Tests/BankAccountServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using Microsoft.Extensions.DependencyInjection;
+using RichardSzalay.MockHttp;
+
+namespace ResilientHttpClients.Services.Tests;
+
+public class BankAccountServiceTests
+{
+    [Fact]
+    public async Task Test1()
+    {
+        var services = new ServiceCollection();
+        services.AddDistributedMemoryCache();
+        services.AddSingleton<TokenHeaderMiddleware>();
+
+        var mockedTokenHttp = new MockHttpMessageHandler();
+        mockedTokenHttp
+            .When(HttpMethod.Get, "https://blah.com/api/token")
+            .Respond(HttpStatusCode.OK, new StringContent("old-token"));
+        mockedTokenHttp
+            .When(HttpMethod.Get, "https://blah.com/api/token")
+            .Respond(HttpStatusCode.OK, new StringContent("new-token"));
+        services
+            .AddHttpClient<ITokenService, TokenService>()
+            .ConfigurePrimaryHttpMessageHandler(() => mockedTokenHttp);
+
+        services
+            .AddHttpClient<IBankAccountService, BankAccountService>()
+            .AddHttpMessageHandler<TokenHeaderMiddleware>();
+    }
+}
