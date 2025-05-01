4d67fc0d620967d12681170cd7ebbd3ab6dfc3fb
Cheranga Hatangala <cheranga@gmail.com>
2025-05-01 10:09:10 +1000

    chore: initial commit to setup the Azure function app

---
+++ articles/IsolatedFunctions/src/OrderProcessorFuncApp/CreateOrderFunction.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace OrderProcessorFuncApp;
+
+public class CreateOrderFunction(ILogger<CreateOrderFunction> logger)
+{
+    [Function(nameof(CreateOrderFunction))]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "orders")] HttpRequestData req,
+        FunctionContext context
+    )
+    {
+        await Task.Delay(TimeSpan.FromSeconds(1));
+        logger.LogInformation("C# HTTP trigger function processed a request");
+        return req.CreateResponse(HttpStatusCode.Accepted);
+    }
+}
---
+++ articles/IsolatedFunctions/src/OrderProcessorFuncApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults(builder =>
+    {
+        builder.Services.Configure<JsonSerializerOptions>(options =>
+        {
+            options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            options.PropertyNameCaseInsensitive = true;
+            options.WriteIndented = false;
+            options.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        });
+    })
+    .ConfigureAppConfiguration(builder =>
+    {
+        builder.AddUserSecrets<Program>();
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+    })
+    .ConfigureLogging(builder =>
+    {
+        builder.AddJsonConsole();
+    })
+    .Build();
+
+await host.RunAsync();
