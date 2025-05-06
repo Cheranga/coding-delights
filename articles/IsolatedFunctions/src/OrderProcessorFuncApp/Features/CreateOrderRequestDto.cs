8083932c40f7dc622d6e107579f667399beff40d
Cheranga <cheranga@gmail.com>
2025-05-07 07:13:18 +1000

    fix: fixing csharpier formattings

--- articles/IsolatedFunctions/src/OrderProcessorFuncApp/Features/CreateOrderRequestDto.cs
+++ articles/IsolatedFunctions/src/OrderProcessorFuncApp/Features/CreateOrderRequestDto.cs
@@ -4,8 +4,8 @@ public sealed record CreateOrderRequestDto
 {
     public required string OrderId { get; init; }
     public required string ReferenceId { get; init; }
-    
+
     public required DateTimeOffset OrderDate { get; init; }
-    
+
     public required IReadOnlyCollection<OrderItem> Items { get; init; }
 }
