d62f7481af866629af8b2facb9c7ab53ea04c3e1
Cheranga <cheranga@gmail.com>
2025-05-08 19:30:18 +1000

    refactor: loading Serilog configuration, conditional provisioning
    * Serilog configuration through appsettings.json file
    
    * Flag to decide to provision Azure resources or not

--- articles/IsolatedFunctions/src/OrderProcessorFuncApp/Program.cs
+++ articles/IsolatedFunctions/src/OrderProcessorFuncApp/Program.cs
@@ -45,55 +45,55 @@ var host = new HostBuilder()
 
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
-        
+
 #pragma warning disable S125
         // services.AddSingleton<ITelemetryInitializer, CustomTelemetryInitializer>();
 #pragma warning restore S125
     })
-    .ConfigureLogging((context,logging) =>
-    {
-        Log.Logger = new LoggerConfiguration()
-            .ReadFrom.Configuration(context.Configuration)
-            .CreateLogger();
-        
-        // Log.Logger = new LoggerConfiguration()
-        //     .MinimumLevel.Information()
-        //     .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-        //     .MinimumLevel.Override("Worker", LogEventLevel.Warning)
-        //     .MinimumLevel.Override("Host", LogEventLevel.Warning)
-        //     .MinimumLevel.Override("System", LogEventLevel.Warning)
-        //     .MinimumLevel.Override("Function", LogEventLevel.Warning)
-        //     .MinimumLevel.Override("Azure*", LogEventLevel.Warning)
-        //     .MinimumLevel.Override("OrderProcessorFuncApp.Features", LogEventLevel.Information)
-        //     .Enrich.FromLogContext()
-        //     .Enrich.WithProperty("FunctionAppName", "OrderProcessorFuncApp")
-        //     .WriteTo.Console()
-        //     .WriteTo.ApplicationInsights(TelemetryConverter.Traces, LogEventLevel.Information)
+    .ConfigureLogging(
+        (context, logging) =>
+        {
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(context.Configuration).CreateLogger();
+
+            // Log.Logger = new LoggerConfiguration()
+            //     .MinimumLevel.Information()
+            //     .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            //     .MinimumLevel.Override("Worker", LogEventLevel.Warning)
+            //     .MinimumLevel.Override("Host", LogEventLevel.Warning)
+            //     .MinimumLevel.Override("System", LogEventLevel.Warning)
+            //     .MinimumLevel.Override("Function", LogEventLevel.Warning)
+            //     .MinimumLevel.Override("Azure*", LogEventLevel.Warning)
+            //     .MinimumLevel.Override("OrderProcessorFuncApp.Features", LogEventLevel.Information)
+            //     .Enrich.FromLogContext()
+            //     .Enrich.WithProperty("FunctionAppName", "OrderProcessorFuncApp")
+            //     .WriteTo.Console()
+            //     .WriteTo.ApplicationInsights(TelemetryConverter.Traces, LogEventLevel.Information)
 #pragma warning disable S125
-        //     .CreateLogger();
+            //     .CreateLogger();
 #pragma warning restore S125
 #pragma warning disable S4663
-        //
+            //
 #pragma warning restore S4663
-        logging.AddSerilog(Log.Logger, true);
+            logging.AddSerilog(Log.Logger, true);
 
-        // Remove the default Application Insights logger provider so that Information logs are sent
-        // https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide?tabs=hostbuilder%2Clinux&WT.mc_id=DOP-MVP-5001655#managing-log-levels
-        logging.Services.Configure<LoggerFilterOptions>(options =>
-        {
-            var defaultRule = options.Rules.FirstOrDefault(rule =>
-                string.Equals(
-                    rule.ProviderName,
-                    "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider",
-                    StringComparison.OrdinalIgnoreCase
-                )
-            );
-            if (defaultRule is not null)
+            // Remove the default Application Insights logger provider so that Information logs are sent
+            // https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide?tabs=hostbuilder%2Clinux&WT.mc_id=DOP-MVP-5001655#managing-log-levels
+            logging.Services.Configure<LoggerFilterOptions>(options =>
             {
-                options.Rules.Remove(defaultRule);
-            }
-        });
-    })
+                var defaultRule = options.Rules.FirstOrDefault(rule =>
+                    string.Equals(
+                        rule.ProviderName,
+                        "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider",
+                        StringComparison.OrdinalIgnoreCase
+                    )
+                );
+                if (defaultRule is not null)
+                {
+                    options.Rules.Remove(defaultRule);
+                }
+            });
+        }
+    )
     .Build();
 
 await host.RunAsync();
