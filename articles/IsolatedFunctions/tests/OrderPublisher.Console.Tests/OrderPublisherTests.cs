2e7547f27a940f7e43e424a32cfdd5082d4832da
Cheranga Hatangala <cheranga@gmail.com>
2025-05-21 19:23:20 +1000

    test: using Bunsen to refactor the tests

--- articles/IsolatedFunctions/tests/OrderPublisher.Console.Tests/OrderPublisherTests.cs
+++ articles/IsolatedFunctions/tests/OrderPublisher.Console.Tests/OrderPublisherTests.cs
@@ -2,7 +2,6 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using AutoBogus;
 using Azure.Messaging.ServiceBus;
-using Azure.Messaging.ServiceBus.Administration;
 using OrderPublisher.Console.Models;
 using OrderPublisher.Console.Services;
 
@@ -45,18 +44,30 @@ public class OrderPublisherTests(ServiceBusFixture serviceBusFixture) : IClassFi
             .Assert((data, result) => result != null && data.msg.Id == result.Id);
 
     [Fact(DisplayName = "Reading from a session enabled subscription")]
-    public async Task Test2()
-    {
-        var connectionString = serviceBusFixture.GetConnectionString();
-        var msg = new AutoFaker<CreateOrderMessage>().Generate();
+    public async Task Test2() =>
+        await Arrange(() =>
+            {
+                var connectionString = serviceBusFixture.GetConnectionString();
 
-        var client = new ServiceBusClient(connectionString);
-        var messagePublisher = new MessagePublisher(client, _serializerOptions);
-        await messagePublisher.PublishToTopicAsync("sbt-orders", msg, CancellationToken.None);
-        var receiver = await client.AcceptSessionAsync("sbt-orders", "sbts-orders", msg.OrderId.ToString());
-        var rec = await receiver.ReceiveMessageAsync(TimeSpan.FromSeconds(2));
-        var recMsg = rec.Body.ToObjectFromJson<CreateOrderMessage>(_serializerOptions);
-        Assert.NotNull(recMsg);
-        Assert.Equal(msg.Id, recMsg.Id);
-    }
+                var clientFunc = () => new ServiceBusClient(connectionString);
+                var messagePublisherFunc = (ServiceBusClient client, JsonSerializerOptions options) =>
+                    new MessagePublisher(client, options);
+                var receiverFunc = (ServiceBusClient client) => client.CreateReceiver("orders");
+
+                var msg = new AutoFaker<CreateOrderMessage>().Generate();
+
+                return (clientFunc, messagePublisherFunc, receiverFunc, msg);
+            })
+            .Act(async data =>
+            {
+                await using var client = data.clientFunc();
+                var publisher = data.messagePublisherFunc(client, _serializerOptions);
+                await publisher.PublishToTopicAsync("sbt-orders", data.msg, CancellationToken.None);
+                await using var receiver = await client.AcceptSessionAsync("sbt-orders", "sbts-orders", data.msg.OrderId.ToString());
+                var rec = await receiver.ReceiveMessageAsync(TimeSpan.FromSeconds(2));
+                var recMessage = rec.Body.ToObjectFromJson<CreateOrderMessage>(_serializerOptions);
+
+                return recMessage;
+            })
+            .Assert((data, result) => result != null && data.msg.Id == result.Id);
 }
